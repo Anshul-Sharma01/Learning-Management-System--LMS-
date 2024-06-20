318346402ac6edffb64bbce3d220bc355f433aee
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-06-20 11:39:29 +0530

    Added action buttons to the sidebar and implemented logic

--- client/src/Layouts/HomeLayout.jsx
+++ client/src/Layouts/HomeLayout.jsx
@@ -1,20 +1,38 @@
 import { FiMenu } from "react-icons/fi";
 import { AiFillCloseCircle } from "react-icons/ai";
 import Footer from "../Components/Footer";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 function HomeLayout({ children }){
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
+
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+    const role = useSelector((state) => state?.auth?.role);
+
+
     function changeWidth(){
         const drawerSide = document.getElementsByClassName('drawer-side');
-        drawerSide[0].style.width = 0;
+        drawerSide[0].style.width = 'auto';
     }
 
     function hideDrawer(){
         const element = document.getElementsByClassName('drawer-toggle');
         element[0].checked = false;
 
-        changeWidth();
+        const drawerSide = document.getElementsByClassName('drawer-side');
+        drawerSide[0].style.width = 0;
+    }
+
+    async function handleLogout(e){
+        e.preventdefault();
+        // const res = await dispatch(logout());
+        // if(res?.payload?.success)
+        navigate("/");
     }
 
     return(
@@ -32,24 +50,49 @@ function HomeLayout({ children }){
                 </div>
                 <div className="drawer-side w-0">
                     <label htmlFor="my-drawer" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-48 sm:w-80 bg-base-100 text-base-content relative">
+                    <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative h-fit">
                         <li className="w-fit absolute right-2 z-50">
                             <button onClick={hideDrawer}>
                                 <AiFillCloseCircle size={24} />
                             </button>
                         </li>
                         <li>
-                            <link to="/">Home</link>
+                            <Link to="/">Home</Link>
+                        </li>
+                        {isLoggedIn && role === 'ADMIN' && (
+                            <li>
+                                <Link to="/admin/dashboard">Admin Dashboard</Link>
+                            </li>
+                        )}
+                        <li>
+                            <Link to="/course">Courses</Link>
                         </li>
                         <li>
-                            <link to="/course">Courses</link>
+                            <Link to="/contact">Contact Us</Link>
                         </li>
                         <li>
-                            <link to="/contact">Contact Us</link>
+                            <Link to="/about">About</Link>
                         </li>
-                        <li>
-                            <link to="/about">About</link>
-                        </li>
+                        {!isLoggedIn && (
+                            <li className="mt-auto w-full flex flex-col items-center">
+                                <button className="btn btn-primary px-4 py-1 font-semibold rounded-md w-full mb-2">
+                                    <Link to="/login">Login</Link>
+                                </button>
+                                <button className="btn btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                                    <Link to="/signup">SignUp</Link>
+                                </button>
+                            </li>
+                        )}
+                        {isLoggedIn && (
+                            <li className="mt-auto w-full flex flex-col items-center">
+                                <button className="btn btn-primary px-4 py-1 font-semibold rounded-md w-full mb-2">
+                                    <Link to="/user/profile">Profile</Link>
+                                </button>
+                                <button className="btn btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                                    <Link onClick={handleLogout} >Logout</Link>
+                                </button>
+                            </li>
+                        )}
                     </ul>
                 </div>
             </div>
