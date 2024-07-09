4d804ecbf55a757ac10a36eb5480d6d6da0a195d
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-07-09 17:42:47 +0530

    created slice for courses

---
+++ client/src/Redux/Slices/CourseSlice.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk, ReducerType } from "@reduxjs/toolkit"
+import toast from "react-hot-toast"
+import axiosInstance from "../../Helpers/axiosInstance"
+
+const initialState = {
+    courseData : [],
+}
+
+
+export const getAllCourses = createAsyncThunk("/course/get", async() => {
+    try{
+
+        const response = axiosInstance.get("/courses");
+        toast.promise(response, {
+            loading : 'Loading course data..',
+            success : 'Courses loaded successfully',
+            error : "Failed to get the courses"
+        })
+
+        return (await response).data.courses;
+
+    }catch(err){
+        toast.error(error?.response?.data?.message);
+    }
+})
+
+
+
+
+const courseSlice = createSlice({
+    name : 'courses',
+    initialState,
+    reducers : {},
+    extraReducers : (builder) => {
+
+    }
+})
+
+
+export default courseSlice.reducer;
